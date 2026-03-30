26b4c1bda581577f02710951ee4cd46d1963fd83
N <910388@cognizant.com>
2026-03-30 16:37:24 +0530

    Updating folder structure

---
+++ pages/HomePage.js
@@ -0,0 +1,17 @@
+const url = "https://demowebshop.tricentis.com/";
+const HomePage_Logo = '//*[@alt="Tricentis Demo Web Shop"]';
+const HeaderLink_Register = '//div[@class="header-links"]//a[@class="ico-register"]';
+const HeaderLink_Login = '//div[@class="header-links"]//a[@class="ico-login"]';
+const HeaderLink_ShopingCart = '//div[@class="header-links"]//a[@class="ico-cart"]';
+const HeaderLink_Wishlist = '//div[@class="header-links"]//a[@class="ico-wishlist"]';
+
+const TopMenu_Books = '//ul[@class="top-menu"]//a[@href="/books"]';
+const TopMenu_Computer = '//ul[@class="top-menu"]//a[@href="/computers"]';
+const TopMenu_Electronics = '//ul[@class="top-menu"]//a[@href="/electronics"]';
+const TopMenu_Apparel = '//ul[@class="top-menu"]//a[@href="/apparel-shoes"]';
+const TopMenu_DigitalDownloads = '//ul[@class="top-menu"]//a[@href="/digital-downloads"]';
+const TopMenu_Jewelry = '//ul[@class="top-menu"]//a[@href="/jewelry"]';
+const TopMenu_GiftCards = '//ul[@class="top-menu"]//a[@href="/gift-cards"]';
+
+const ContentHeader = '//div[@class="topic-html-content"]//h2[@class="topic-html-content-header"]';
+const ContentBody ='//div[@class="topic-html-content"]//div[@class="topic-html-content-body"]';
